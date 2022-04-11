77d84401ff659e907dd596465f85e972e7586cff
jinkorotan <hiraikazuko@yahoo.co.jp>
2022-04-11 16:32:32 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,24 @@
 class RoomsController < ApplicationController
   def new
-    @room = Room.new
+    @room = Room.new(room_params)
+    if @rooms.save
+        redirect_to root_path
+     else
+        render :new
+    end
+  end
+
+  def create
+    if @rooms.save
+      redirect_to root_path
+   else
+      render :new
+  end
+end
+
+private
+
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
   end
 end
